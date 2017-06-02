00d6e5a9e10b8489eb0c68897fcba8e70ed1dfa6
airCstnr <raphael.castanier@free.fr>
2017-06-02 13:45:19 +0200

    Correction de la list view, ajout de Bouton Anonyme, ajout de choix exercices

--- app/src/main/java/aircstnr/loustics/Connexion.java
+++ app/src/main/java/aircstnr/loustics/Connexion.java
@@ -1,11 +1,16 @@
 package aircstnr.loustics;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -27,8 +32,37 @@ public class Connexion extends AppCompatActivity implements AdapterView.OnItemCl
         listView = (ListView) findViewById(R.id.listViewUtilisateurs);
 
         // création d'un adapter pour la liste des utilisateurs
-        ArrayAdapter<Utilisateur> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, utilisateurs);
+        //ArrayAdapter<Utilisateur> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, utilisateurs);
 
+        // Solution customisée
+        ArrayAdapter<Utilisateur> adapter = new ArrayAdapter<Utilisateur>(this, R.layout.activity_utilisateur_ligne_connexion, utilisateurs) {
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                // Inflate LAYOUT
+                if(convertView == null){
+                    LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    convertView = (ViewGroup)inflater.inflate(R.layout.activity_utilisateur_ligne_connexion, null);
+                }
+
+                // VIEW
+                TextView text1 = (TextView) convertView.findViewById(R.id.text1);
+                TextView text2 = (TextView) convertView.findViewById(R.id.text2);
+
+                // Charger la vue avec les données
+                Utilisateur Utilisateur = utilisateurs.get(position);
+                text1.setText(Utilisateur.getNom());
+                text2.setText(Utilisateur.getPrenom());
+
+                //
+                return convertView;
+            }
+        };
+        
+        
+        
+        
         // attribution de l'adapter à la listView
         listView.setAdapter(adapter);
 
